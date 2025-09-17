938d1f26f62d2bdc447f8483add2dcd9da560c43
agenext <lucasviniciussoaresdasilva100@gmail.com>
2025-09-17 00:34:08 -0300

    Delete Translate.lua

--- Translate.lua
+++
@@ -1,6 +0,0 @@
-Translations = {
-    ["BR"] = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/agenext/Translations/refs/heads/main/Languages/PT-BR.txt"))(),
-    ["TH"] = {},
-    ["VN"] = {},
-
-}
