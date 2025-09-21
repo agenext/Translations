cc92dfaa419efe52c98abe3b1e27b9dcb81c95f8
agenext <lucasviniciussoaresdasilva100@gmail.com>
2025-09-20 21:55:49 -0300

    Add files via upload

--- SomeoneHub/Languages/Portuguese.lua
+++ SomeoneHub/Languages/Portuguese.lua
@@ -1,3 +1,13 @@
 	return {
-		["Full Moon In % Hours"] = "Lua Cheia Em % Horas",
+		["Someone Hub | Anime Eternal"] = "Alguém Hub | Anime Eterno",
+		["Social Media"] = "Mídias Sociais",
+		["Main"] = "Principal",
+		["Fruits"] = "Frutas",
+		["Teleport"] = "Teletransporte",
+		["Player"] = "Jogador",
+		["Shop"] = "Comprar",
+		["Visual"] = "Visual",
+		["Settings"] = "Configurações",
+		["Main Settings"] = "Configurações Principais",
+		["Auto Attack"] = "Ataque Automático",
 	}
