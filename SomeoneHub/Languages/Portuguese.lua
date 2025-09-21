7f41274c3bacec6867df0fbce62b09639a01534b
agenext <lucasviniciussoaresdasilva100@gmail.com>
2025-09-21 20:20:47 -0300

    Update Portuguese.lua

--- SomeoneHub/Languages/Portuguese.lua
+++ SomeoneHub/Languages/Portuguese.lua
@@ -1,13 +1,3 @@
 	return {
-		["Someone Hub | Anime Eternal"] = "Alguém Hub | Anime Eterno",
-		["Social Media"] = "Mídias Sociais",
-		["Main"] = "Principal",
-		["Fruits"] = "Frutas",
-		["Teleport"] = "Teletransporte",
-		["Player"] = "Jogador",
-		["Shop"] = "Comprar",
-		["Visual"] = "Visual",
-		["Settings"] = "Configurações",
-		["Main Settings"] = "Configurações Principais",
-		["Auto Attack"] = "Ataque Automático",
+		["Tab Farm Main"] = "Farm Principal",
 	}
